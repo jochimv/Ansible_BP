8de17490e5de3c969938b5dbfa42ed637af27385
Václav Jochim <jocv00@vse.cz>
2023-05-01 19:44:27 +0200

    Fix bug that made backend unreachable from frontend

--- packages/frontend/src/constants/index.ts
+++ packages/frontend/src/constants/index.ts
@@ -3,7 +3,7 @@
  * Date: 2023-04-28
  */
 
-export const BE_IP_ADDRESS = process.env.NEXT_PUBLIC_STAGE === 'development' ? 'localhost' : 'backend';
+export const BE_IP_ADDRESS = process.env.NEXT_PUBLIC_STAGE === 'development' ? 'localhost' : '127.0.0.1';
 
 export const BE_BASE_URL = `http://${BE_IP_ADDRESS}:4000`;
 
